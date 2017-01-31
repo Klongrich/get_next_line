fdcd542b97ae689e3bb0ef64d5215e4ed3b5c841
Kyle LONGRICH <klongric@e1z2r6p11.42.us.org>
2017-01-31 04:37:17 -0800

    Here it is

---
+++ get_next_line.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klongric <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/29 22:38:10 by klongric          #+#    #+#             */
+/*   Updated: 2017/01/30 00:48:40 by klongric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		checkend(char **list, int i)
+{
+	if (i != 0)
+	{
+		(*list)[i] = '\0';
+		return (1);
+	}
+	return (0);
+}
+
+void	swap(char **list, int i, char *temp)
+{
+	temp = *list;
+	*list = (char *)malloc(sizeof(char) * i + 1);
+	ft_memcpy(*list, temp, i);
+	free(temp);
+}
+
+int		get_next_line(const int fd, char **list)
+{
+	char			buf[BUFF_SIZE];
+	char			*temp;
+	static int		i;
+
+	i = 0;
+	temp = 0;
+	if (!list || read(fd, buf, 0) < 0)
+		return (-1);
+	if (!(*list = (char *)malloc(sizeof(char) * 1)))
+		return (-1);
+	while ((read(fd, buf, 1) > 0))
+	{
+		if (buf[0] == '\n')
+		{
+			(*list)[i] = '\0';
+			return (1);
+		}
+		swap(list, i, temp);
+		(*list)[i] = buf[0];
+		i++;
+	}
+	return (checkend(list, i));
+}
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klongric <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/29 22:38:43 by klongric          #+#    #+#             */
+/*   Updated: 2017/01/30 00:32:11 by klongric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <stdlib.h>
+# define BUFF_SIZE 45
+
+int	get_next_line(const int fd, char **list);
+
+#endif
